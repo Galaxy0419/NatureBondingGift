d60f04ec92a1178ecb1878a192dfa2fc613e8def
Tianchen Tang <ttan0037@student.monash.edu>
2021-04-09 19:22:44 +0800

    categories/view: Remove view action

--- templates/Categories/index.php
+++ templates/Categories/index.php
@@ -22,7 +22,6 @@
                     <td><?= $this->Number->format($category->id) ?></td>
                     <td><?= h($category->name) ?></td>
                     <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $category->id]) ?>
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $category->id]) ?>
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $category->id], ['confirm' => __('Are you sure you want to delete # {0}?', $category->id)]) ?>
                     </td>
