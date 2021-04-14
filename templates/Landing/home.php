972a59a08a1b95ba542b9c1ad3422fe51e2b29c7
Tanish Naik <tnai0005@student.monash.edu>
2021-04-14 19:04:49 +1000

    Added function to retrieve and display images

--- templates/Landing/home.php
+++ templates/Landing/home.php
@@ -9,8 +9,10 @@
         <h2 class="title">All Photos</h2>
         <div class="row">
             <div class="col-4">
-                <?php foreach ($photos as $photo){ ?>
+                <?php foreach ($photos as $photo){
+                    $imagePath = WATERMARK_PHOTO_PATH."/".$photo->file_name; ?>
 <!--                <img src="images/photo1.png">-->
+                <?php echo $this->Html->image($imagePath)?>
                 <h4><?php echo $photo->name; ?></h4>
                 <p>$<?php echo $photo->price; ?></p>
 
