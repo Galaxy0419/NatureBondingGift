4f1345e154dae72250c96d8ac0ab9ae07401de95
Tanish Naik <tnai0005@student.monash.edu>
2021-04-14 18:40:45 +1000

    Added code that retrieve name and price from db and displays it

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Controller;
+/**
+* @property \App\Model\Table\PhotosTable $Photos
+* @method \App\Model\Entity\Photo[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+    */
 
 class LandingController extends AppController{
-   
+
     public function home()
     {
         $this->viewBuilder()->setLayout('bones');
+        $this->loadModel('Photos');
+        $photos= $this->Photos->find();
+        $this->set('photos',$photos);
+
     }
 }
--- templates/Landing/home.php
+++ templates/Landing/home.php
@@ -1,41 +1,21 @@
-<!---------Products Section-------------->    
-
+<!---------Products Section-------------->
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Photo[]|\Cake\Collection\CollectionInterface $photos
+ */
+?>
 <div class="small-container">
         <h2 class="title">All Photos</h2>
         <div class="row">
             <div class="col-4">
-                <img src="images/photo1.png">
-                <h4>Lake</h4>
-                <p>$15.00</p>
+                <?php foreach ($photos as $photo){ ?>
+<!--                <img src="images/photo1.png">-->
+                <h4><?php echo $photo->name; ?></h4>
+                <p>$<?php echo $photo->price; ?></p>
+
+                <?php } ?>
             </div>
 
-        <div class="col-4">
-                <img src="images/photo2.png">
-                <h4>Forest</h4>
-                <p>$20.00</p>
-            </div>
-
-         <div class="col-4">
-                <img src="images/photo3.png">
-                <h4>Field</h4>
-                <p>$17.00</p>
-            </div>
-         <div class="col-4">
-                <img src="images/photo4.png">
-                <h4>Butterfly</h4>
-                <p>$10.00</p>
-            </div>
-         <div class="col-4">
-                <img src="images/photo5.png">
-                <h4>Leaves</h4>
-                <p>$13.00</p>
-            </div> 
-         <div class="col-4">
-                <img src="images/photo6.png">
-                <h4>River</h4>
-                <p>$25.00</p>
-            </div> 
-           
         </div>
     </div>
-    
