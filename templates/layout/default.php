cccbcfe84f913e9700a4f8688cb29f9a0066fed7
rsal0004 <rsal0004@student.monash.edu>
2021-04-16 17:10:38 +1000

    Replace default.php

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -36,22 +36,25 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
-            <?= $this->fetch('content') ?>
-        </div>
-    </main>
-    <footer>
-    </footer>
+<nav class="top-nav">
+    <div class="top-nav-title">
+        <a  target="_self" rel="noopener" href=http://localhost:8765/landing/home>
+        <img src="/img/nbg-logo.png" alt="Natures Bonding Gift" style="width:136px;height:90px;" >
+        </a>
+    </div>
+    <div class="top-nav-links">
+        <a target="_self" rel="noopener" href=http://localhost:8765/photos>Photos</a>
+        <a target="_self" rel="noopener" href="http://localhost:8765/categories">Categories</a>
+    </div>
+</nav>
+<main class="main">
+    <div class="container">
+        <?= $this->Flash->render() ?>
+        <?= $this->fetch('content') ?>
+    </div>
+</main>
+<footer>
+</footer>
+
 </body>
 </html>
