8baa05236237c7a57d2f0f4ca5e8d8f12521d60c
Tianchen Tang <ttan0037@student.monash.edu>
2021-04-29 17:01:59 +0800

    enquiries/add: Remove unnecessary UI elements

--- templates/Enquiries/add.php
+++ templates/Enquiries/add.php
@@ -4,29 +4,19 @@
  * @var \App\Model\Entity\Enquiry $enquiry
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Enquiries'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="enquiries form content">
-            <?= $this->Form->create($enquiry) ?>
-            <fieldset>
-                <legend><?= __('Add Enquiry') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('email');
-                    echo $this->Form->control('subject');
-                    echo $this->Form->control('description');
-                    echo $this->Form->control('status');
-                    echo $this->Form->control('create_date');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
+<div class="column-responsive column-80">
+    <div class="enquiries form content">
+        <?= $this->Form->create($enquiry) ?>
+        <fieldset>
+            <legend><?= __('Add Enquiry') ?></legend>
+            <?php
+                echo $this->Form->control('name');
+                echo $this->Form->control('email');
+                echo $this->Form->control('subject');
+                echo $this->Form->control('description', ['type' => 'textarea', 'style' => 'height: 256px']);
+            ?>
+        </fieldset>
+        <?= $this->Form->button(__('Submit')) ?>
+        <?= $this->Form->end() ?>
     </div>
 </div>
