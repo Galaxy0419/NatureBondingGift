7c6bcd5ed058a77a0af80f6cb4cb84eeab8efeb8
Tianchen Tang <ttan0037@student.monash.edu>
2021-05-06 14:42:49 +0800

    Format LandingController.php

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -1,26 +1,31 @@
 <?php
 
 namespace App\Controller;
+
 /**
-* @property \App\Model\Table\PhotosTable $Photos
-* @method \App\Model\Entity\Photo[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
-    */
+ * @property \App\Model\Table\PhotosTable $Photos
+ * @method \App\Model\Entity\Photo[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
 
-class LandingController extends AppController{
-
+class LandingController extends AppController
+{
     public function home()
     {
-        $this->viewBuilder()->setLayout('bones'); //uses bones.php in layout folder as the base layout.
-        $this->loadModel('Photos'); //loading Photos model to retrieve photo data from the database.
-        $photos= $this->Photos->find('all',['contain'=>['Categories']])->toArray();
-        /*retrieves all entities (records) from the Photos table and stores in the variable photos.
-        Additionally, finds the category details for each Photos record and converts the variable 'photos' to an array.
+        /* Use bones.php in layout folder as the base layout. */
+        $this->viewBuilder()->setLayout('bones');
+        /* Load Photos model to retrieve photo data from the database. */
+        $this->loadModel('Photos');
+
+        /**
+         * Retrieve all entities (records) from the Photos table and stores in the variable photos.
+         * Additionally, finds the category details for each Photos record and converts the variable 'photos' to an array.
          */
- $this->set('photos',$photos); //sends the photos variable to the view (home.php) as photos.
+        $photos = $this->Photos->find('all', ['contain' => ['Categories']])->toArray();
+        $this->set('photos', $photos);
     }
 
     public function about()
     {
-    $this->viewBuilder()->setLayout('bones'); //uses bones.php in layout folder as the base layout.
+        $this->viewBuilder()->setLayout('bones'); //uses bones.php in layout folder as the base layout.
     }
 }
