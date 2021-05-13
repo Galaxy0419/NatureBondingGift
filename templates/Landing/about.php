02a79c92f77f1a1dd3b3951507c8f2d4bd5d7081
tnai0005 <tnai0005@student.monash.edu>
2021-05-13 23:50:22 +1000

    Added business description in the about page.

--- templates/Landing/about.php
+++ templates/Landing/about.php
@@ -1,2 +1,20 @@
 <h2 class="title">About</h2>
-<p> Welcome to Nature's Bonding Gifts</p>
+<p class="about"> Nature's Bonding Gifts was founded by Pooneh Zarbafian in 20xx [to be updated by client]. Pooneh is a
+    nature aficionado and wanted to share her passion with
+    fellow nature lovers by sharing the pictures of nature she captures whenever she has the time to go exploring,
+    leading to the conception of
+    Natures Bonding Gifts. Nature's Bonding Gifts sells high-resolution, vibrant and rejuvenating pictures of nature
+    that will look just as
+    good on fabrics like canvas or linen as they will on your desktop!
+</p>
+<br>
+<br>
+<br>
+<br>
+
+
+<style>
+    p.about {
+        font-size: 25px;
+    }
+</style>
